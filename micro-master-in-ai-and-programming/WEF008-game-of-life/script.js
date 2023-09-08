f0ccdf26ad8b341f526a3f44afcb7b20a565dcef
Tien Hanprab <tienhanprab@gmail.com>
2023-09-09 05:01:31 +0800

    progress

--- micro-master-in-ai-and-programming/WEF008-game-of-life/script.js
+++ micro-master-in-ai-and-programming/WEF008-game-of-life/script.js
@@ -1,12 +1,20 @@
-//Use DOM to handle the active class change
-document.addEventListener('DOMContentLoaded', function() {
+// Use DOM to handle the active class change
+document.addEventListener('DOMContentLoaded', () => {
     var navLinks = document.querySelectorAll('.navbar-nav .nav-link');
-    navLinks.forEach(function(link) {
-        link.addEventListener('click', function() {
+    navLinks.forEach((link) => {
+        link.addEventListener('click', () => {
             navLinks.forEach(function(navLinks) {
                 navLinks.classList.remove('active');
             });
             this.classList.add('active');
         });
     });
+});
+
+// Scroll Animation
+window.addEventListener('scroll', () => {
+    document.body.style.setProperty(
+        '--scroll',
+        window.scrollY / (document.body.offsetHeight - window.innerHeight)
+    );
 });
