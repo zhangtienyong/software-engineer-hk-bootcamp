24c171d65ed84135f266638f2f468e139a126920
Tien Hanprab <tienhanprab@gmail.com>
2023-09-10 04:20:23 +0800

    Intersection Observer API

--- micro-master-in-ai-and-programming/WEF008-game-of-life/script.js
+++ micro-master-in-ai-and-programming/WEF008-game-of-life/script.js
@@ -11,10 +11,25 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
-// Scroll Animation
+// Progress
 window.addEventListener('scroll', () => {
     document.body.style.setProperty(
         '--scroll',
         window.scrollY / (document.body.offsetHeight - window.innerHeight)
     );
 });
+
+// IntersectionObserver
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry);
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
