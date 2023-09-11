0d740a5b20fe30116e0a37f8d70e42526097be52
Tien Hanprab <tienhanprab@gmail.com>
2023-09-11 11:22:01 +0800

    edit navbar

--- micro-master-in-ai-and-programming/WEF008-game-of-life/script.js
+++ micro-master-in-ai-and-programming/WEF008-game-of-life/script.js
@@ -1,35 +1,18 @@
-// Use DOM to handle the active class change
 document.addEventListener('DOMContentLoaded', () => {
-    var navLinks = document.querySelectorAll('.navbar-nav .nav-link');
+
+    const navLinks = document.querySelectorAll('.navbar-nav .nav-link');
+
+    // Navbar
     navLinks.forEach((link) => {
-        link.addEventListener('click', () => {
-            navLinks.forEach(function(navLinks) {
-                navLinks.classList.remove('active');
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+            
+            navLinks.forEach((navLink) => {
+                navLink.classList.remove('active');
             });
-            this.classList.add('active');
+
+            link.classList.add('active');
         });
     });
-});
 
-// Progress
-window.addEventListener('scroll', () => {
-    document.body.style.setProperty(
-        '--scroll',
-        window.scrollY / (document.body.offsetHeight - window.innerHeight)
-    );
 });
-
-// IntersectionObserver
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry);
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show');
-        } else {
-            entry.target.classList.remove('show');
-        }
-    });
-});
-
-const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach((el) => observer.observe(el));
