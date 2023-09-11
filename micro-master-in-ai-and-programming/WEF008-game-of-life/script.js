fbeb6deb02643deb8a448f77bd9a0c224fffd400
Tien Hanprab <tienhanprab@gmail.com>
2023-09-11 14:30:16 +0800

    navigation bar scroll down

--- micro-master-in-ai-and-programming/WEF008-game-of-life/script.js
+++ micro-master-in-ai-and-programming/WEF008-game-of-life/script.js
@@ -1,18 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
-
     const navLinks = document.querySelectorAll('.navbar-nav .nav-link');
 
     // Navbar
     navLinks.forEach((link) => {
         link.addEventListener('click', (e) => {
             e.preventDefault();
-            
-            navLinks.forEach((navLink) => {
-                navLink.classList.remove('active');
-            });
 
-            link.classList.add('active');
+            const targetId = link.getAttribute('href');
+            const targetSection = document.querySelector(targetId);
+
+            if (targetSection) {
+                window.scrollTo({
+                    top: targetSection.offsetTop,
+                    behavior: 'smooth'
+                });
+
+                navLinks.forEach((navLink) => {
+                    navLink.classList.remove('active');
+                });
+
+                link.classList.add('active');
+            }
         });
     });
-
 });
