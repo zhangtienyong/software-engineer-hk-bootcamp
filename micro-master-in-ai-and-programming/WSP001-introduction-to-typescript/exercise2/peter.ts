025dbc64a1a822e07ad66dcbcfd3b69d54b1d7f8
Tien Hanprab <tienhanprab@gmail.com>
2023-09-15 17:39:20 +0800

    work

--- micro-master-in-ai-and-programming/WSP001-introduction-to-typescript/exercise2/peter.ts
+++ micro-master-in-ai-and-programming/WSP001-introduction-to-typescript/exercise2/peter.ts
@@ -1,4 +1,4 @@
-/*
+/* Javascript
 const peter = {
   name: "Peter",
   age: 50,
@@ -14,7 +14,12 @@ const peter = {
 };
 */
 
-const peter: { name: string, age: number, students: { name: string, age: number, exercises?: { score: number, date: Date }[] }[] } = {
+// TypeScript
+// parameter?: type | undefine
+const peter: { 
+  name: string, 
+  age: number, 
+  students: { name: string, age: number, exercises?: { score: number, date: Date }[] }[] } = {
     name: "Peter",
     age: 50,
     students: [
@@ -26,4 +31,45 @@ const peter: { name: string, age: number, students: { name: string, age: number,
         exercises: [{ score: 60, date: new Date("2019-01-05") }],
       },
     ],
-  };
+};
+
+
+// parameter: type | undefined = undefined
+interface Exercise {
+  score: number;
+  date: Date;
+}
+
+interface Student {
+  name: string;
+  age: number;
+  exercises?: Exercise[]; // Optional property
+}
+
+interface Person {
+  name: string;
+  age: number;
+  students: Student[];
+}
+
+const peter1: Person = {
+  name: "Peter",
+  age: 50,
+  students: [
+    { name: "Andy", age: 20 },
+    { name: "Bob", age: 23 },
+    {
+      name: "Charlie",
+      age: 25,
+      exercises: [{ score: 60, date: new Date("2019-01-05") }],
+    },
+  ],
+};
+
+
+console.log(peter.students[0].name);
+console.log(peter.students[2]);
+
+// we use optional chaining
+// ? to access potentially undefined properties
+// ?? to provide a default value if the property is undefined
